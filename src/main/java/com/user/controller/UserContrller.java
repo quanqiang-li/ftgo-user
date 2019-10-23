f112c4c4589f04a1890ff03c4de51679b447899a
unknown <2476056494@qq.com>
2019-10-23 08:55:16 +0800

    增加睡眠时间，方便调试

--- src/main/java/com/user/controller/UserContrller.java
+++ src/main/java/com/user/controller/UserContrller.java
@@ -5,7 +5,6 @@ import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,6 +35,12 @@ public class UserContrller {
 	 */
 	@PostMapping("/validUser/{userId}")
 	public String validUser(@PathVariable("userId") String userId, HttpServletRequest req) {
+		try {
+			// 睡眠2秒，方便调试
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		// 得到客户端ip，并记录log
 		String ipAddr = WebUtil.getIpAddr(req);
 		String info = String.format("这里是%s,接收到来自%s的请求,参数userId=%s", host, ipAddr, userId);
